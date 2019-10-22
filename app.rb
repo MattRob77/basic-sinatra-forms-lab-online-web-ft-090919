dd0cdf5973154ed7fdca8c0b7a8ed76c6ac5086a
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-22 12:20:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,15 +1,14 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-  
-  get '/newteam' do 
-    erb :newteam
-  end 
-  
-  get '/team' do 
-    @teams = params 
-    erb :team
-  end 
 
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post '/team' do
+        @team = params
+        erb :team
+    end
 
 end
